9600606ff288143bd5ef1e6754b3c96f3a449d6c
1103426 <1103426@DASOF617E2>
2018-05-14 20:44:11 -0300

    Vesao inicial do projeto

---
+++ Aulas/Aula1105_POO/Aluno.cs
@@ -0,0 +1,28 @@
+﻿namespace Aula1105_POO
+{
+    class Aluno : Pessoa
+    {
+        // Construtores
+        public string Matricula { get; set; }
+
+        public Aluno()
+        {
+
+        }
+
+        public Aluno(string nome) : base(nome)
+        {
+            
+        }
+
+        public Aluno(string nome, string cpf) : base(nome, cpf)
+        {
+                        
+        }
+
+        public override string ShowInfoPessoa()
+        {
+            return "Aluno" + Nome + ", Cpf " + Cpf + ", Matricula " + Matricula;
+        }
+    }
+}
---
+++ Aulas/Aula1105_POO/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Aula1105_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a = new Aluno();
+
+            a.Nome = "Godofredo"; // set
+            //a.Cpf = "12345678909";
+            a.Matricula = "101010";
+            Console.WriteLine("Aluno:" + a.Nome); // get
+
+            Aluno b = new Aluno("Julinda", "123");
+            b.Matricula = "696969";
+
+            Console.WriteLine(b.ShowInfoPessoa());
+
+            Aluno c = new Aluno("Janacreia");
+            c.Matricula = "504531";
+            Console.WriteLine(c.ShowInfoPessoa());
+            //Pessoa p = new Pessoa();
+
+            Console.ReadKey();
+            
+        }
+    }
+}
