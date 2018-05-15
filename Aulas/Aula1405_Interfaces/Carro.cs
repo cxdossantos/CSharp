079a8d4b69fe0aca205f23eeafa14ac87ef0438e
1103426 <1103426@DASOF617E2>
2018-05-14 22:23:51 -0300

    LAzzy Loading

--- Aulas/Aula1405_Interfaces/Carro.cs
+++ Aulas/Aula1405_Interfaces/Carro.cs
@@ -9,6 +9,15 @@ namespace Aula1405_Interfaces
     class Carro : IMeioTransporte
     {
         public int Velocidade { get; set; }
+
+        //chave para Classe Marca
+        public int MarcaId { get; set; }
+
+        //chave para o objeto relacionado (Lazyloading)
+        public virtual Marca _Marca { get; set; }
+
+        public int Nome { get; set; }
+
         public void Acelerar()
         {
             Velocidade += 10;
